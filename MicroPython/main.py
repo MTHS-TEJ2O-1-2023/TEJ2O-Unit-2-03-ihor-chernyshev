3b92596e5f35cfa6f7678e93e1f6212d676e00af
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-09-20 17:13:32 +0000

    fix

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,17 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Ihor Chernyshev
+Created on: Sep 2023
+This program calculates the area and perimeter of a rectangle with dimensions 5 cm & 3 cm
 """
 
 from microbit import *
+from time import sleep
 
 
-display.scroll("Hello, World!")
+display.clear()
+sleep(1)
+
+display.scroll("Perimeter = " + str(2 * 5 + 2 * 3))
+display.clear()
+sleep(1)
+display.scroll("Area = " + str(5 * 3))
