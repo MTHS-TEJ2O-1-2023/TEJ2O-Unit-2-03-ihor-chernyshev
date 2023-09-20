02908d45df208a35a16af0e6ebefd25a68fe5bbb
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-09-20 13:29:11 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -8,7 +8,10 @@
 basic.clearScreen()
 basic.pause(1000)
 
-basic.showString('Perimeter = ' + (2 * (5 + 3)).toString())
+basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
 basic.clearScreen()
 basic.pause(1000)
-basic.showString('Area = ' + (5 * 3).toString())
+basic.showString('The perimeter would be:' + (2 * (5 + 3)).toString())
+basic.clearScreen()
+basic.pause(1000)
+basic.showString('The area would be: ' + (5 * 3).toString())
